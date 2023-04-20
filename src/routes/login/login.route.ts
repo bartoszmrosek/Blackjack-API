3afd418b0c6d913cf18be7f9468ae47358640f2e
Bartosz <bartoszmrosek@op.pl>
2023-04-20 18:02:44 +0200

    Updated folder and files name casing

---
+++ src/routes/login/login.route.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, Router } from 'express';
+import bcrypt from 'bcrypt';
+import mysqlDataSrc from '../../database/mysql.config.js';
+import User from '../../entity/user.entity.js';
+
+const loginRouter = Router();
+
+loginRouter.post('/', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password) return res.sendStatus(400);
+  try {
+    const userRepo = mysqlDataSrc.getRepository(User);
+    const user = await userRepo.findOneBy({ username });
+    if (!user) return res.sendStatus(404);
+    const doPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!doPasswordMatch) return res.sendStatus(401);
+    return jwt.sign({ username }, process.env.SECRET_KEY, { expiresIn: '24h' }, (error, token) => {
+      if (error) return res.sendStatus(500);
+      res.cookie('token', token, {
+        sameSite: 'none', httpOnly: true, secure: true, expires: new Date(Date.now() + 3600000 * 24),
+      });
+      return res.sendStatus(200);
+    });
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+});
+
+export default loginRouter;
---
+++ src/routes/register/register.route.ts
@@ -0,0 +1,21 @@
+import { Request, Response, Router } from 'express';
+import bcrypt from 'bcrypt';
+import mysqlDataSrc from '../../database/mysql.config.js';
+import User from '../../entity/user.entity.js';
+
+const registerRouter = Router();
+
+registerRouter.post('/', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password) return res.sendStatus(400);
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = mysqlDataSrc.getRepository(User).create({ username, password: passwordHash });
+    const results = await mysqlDataSrc.getRepository(User).save(user);
+    return res.send(results);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
+export default registerRouter;
