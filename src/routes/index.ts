9b2e725d50f45c7c22414d342b83dc30b08084d2
Bartosz <bartoszmrosek@op.pl>
2023-04-21 17:11:47 +0200

    Added logout router to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import registerRouter from './register/register.route.js';
 import loginRouter from './login/login.route.js';
 import roomsRouter from './rooms/rooms.route.js';
+import logoutRouter from './logout/logout.route.js';
 
 const router = Router();
 router.use('/register', registerRouter);
 router.use('/login', loginRouter);
+router.use('/logout', logoutRouter);
 router.use('/rooms', roomsRouter);
 
 export default router;
