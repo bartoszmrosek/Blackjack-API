53f7cb6be5d00be7d9da07ab5b36ec5d644cd20f
Bartosz <bartoszmrosek@op.pl>
2023-05-23 13:56:30 +0200

    Added more auth tests

--- src/routes/rooms/rooms.spec.ts
+++ src/routes/rooms/rooms.spec.ts
@@ -13,17 +13,31 @@ describe('Rooms REST endpoint', () => {
     expect(res.status).toBe(200);
     expect(res.body).toStrictEqual(roomsArray);
   });
-  it('on POST /create route should create new room when authorized', async () => {
-    const res = await request(app)
-      .post(`${ROOMS_ROUTE}/create`)
-      .set('Cookie', [`token=${global.token}`]);
-    expect(res.body).toHaveProperty('id');
+  describe('authorized requests', () => {
+    it('on POST /create route should create new room when authorized', async () => {
+      const res = await request(app)
+        .post(`${ROOMS_ROUTE}/create`)
+        .set('Cookie', [`token=${global.token}`]);
+      expect(res.body).toHaveProperty('id');
+    });
+    it('new room id should be retrieved on GET route after creation', async () => {
+      const postRes = await request(app)
+        .post(`${ROOMS_ROUTE}/create`)
+        .set('Cookie', [`token=${global.token}`]);
+      const getRes = await request(app).get(`${ROOMS_ROUTE}/`);
+      expect(getRes.body[1]).toMatchObject(postRes.body);
+    });
   });
-  it('new room id should be retrieved on GET route after creation', async () => {
-    const postRes = await request(app)
-      .post(`${ROOMS_ROUTE}/create`)
-      .set('Cookie', [`token=${global.token}`]);
-    const getRes = await request(app).get(`${ROOMS_ROUTE}/`);
-    expect(getRes.body[1]).toMatchObject(postRes.body);
+  describe('uncomplete and unauthorized requests', () => {
+    it('should return 401 if no token is found', async () => {
+      const res = await request(app).post(`${ROOMS_ROUTE}/create`);
+      expect(res.statusCode).toBe(401);
+    });
+    it('should return 400 if token is wrong', async () => {
+      const res = await request(app)
+        .post(`${ROOMS_ROUTE}/create`)
+        .set('Cookie', ['token=WRONGTOKEN']);
+      expect(res.statusCode).toBe(400);
+    });
   });
 });
